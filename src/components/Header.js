60f710bbfbc7c1779fd43244d3607363e6d52e82
chrisfan0831 <chrisfan0831@gmail.com>
2022-07-20 03:48:05 -0700

    done with navbar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,56 @@ import avatar from "../images/avatar.png"
 import {BiSearch} from "react-icons/bi"
 
 export default function Header() {
-    return (
-        <header>
-            <div className="search-bar">
-                <BiSearch class="search-icon" size={20}/>
-                <input class="search" type="search" placeholder="Search.."></input>
-            </div>
-            <div className="user-identification">
-                <p className="username" style={{marginRight:"10px"}}>login/signup</p>
-                <img class="profile-picture" src={avatar} alt="avatar" width="50" style={{borderRadius:"50px"}}></img>
-            </div>
-        </header>
-    )
+
+    const [menu, setMenu] = React.useState("false")
+    function profileClick() {
+        if(menu == "false") {
+            setMenu("true")
+        }
+        else{
+            setMenu("false")
+        }
+        console.log(menu)
+    }
+
+    if(menu == "true") {
+        return (
+            <header>
+                <div className="search-bar">
+                    <BiSearch class="search-icon" size={20}/>
+                    <input class="search" type="search" placeholder="Search.."></input>
+                </div>
+                <div className="profile">
+                    <div className="profile-brief">
+                        <p className="username" style={{marginRight:"10px"}}>login/signup</p>
+                        <img onClick={profileClick} class="profile-picture" src={avatar} alt="avatar" width="50" style={{border:"solid black 2px"}}></img>
+                    </div>
+                    {profileClick}
+                    <div className="profile-menu">
+                        <p>team-perry@sdsc.com</p>
+                        <p className="menu-choice">Edit Profile</p>
+                        <p className="menu-choice">Switch Account</p>
+                        <p className="menu-choice">Log Out</p>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+    else {
+        return (
+            <header>
+                <div className="search-bar">
+                    <BiSearch class="search-icon" size={20}/>
+                    <input class="search" type="search" placeholder="Search.."></input>
+                </div>
+                <div className="profile">
+                    <div className="profile-brief">
+                        <p className="username" style={{marginRight:"10px"}}>login/signup</p>
+                        <img onClick={profileClick} class="profile-picture" src={avatar} alt="avatar" width="50"></img>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+    
 }
--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,3 +1,4 @@
+import React from "react"
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { SidebarHeader, SidebarFooter, SidebarContent } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
@@ -6,36 +7,88 @@ import {TbDashboard, TbHome} from 'react-icons/tb';
 import {MdOutlineExplore} from "react-icons/md"
 import {FiSettings, FiHelpCircle} from "react-icons/fi"
 
-export default function SideNavBar() {
-    return (
-        <ProSidebar >
-            <SidebarHeader>
-                <img src={sdscLogoRed} alt="logo" height="50" style={{margin:"15px", border:"solid #1d1d1d", borderRadius:"12px"}}/>
-            </SidebarHeader>
-            <SidebarContent>
-                <Menu iconShape="circle" popperArrow="true">
-                    <MenuItem icon={<TbDashboard size={20}/>}>Dashboard</MenuItem>
-                    <SubMenu title="Home" icon={<TbHome size={20}/>}>
-                        <MenuItem>Current Service</MenuItem>
-                        <MenuItem>Service History</MenuItem>
-                        <MenuItem>Saved</MenuItem>
-                    </SubMenu>
-                    <SubMenu title="Explore" icon={<MdOutlineExplore size={20}/>}>
-                        <MenuItem>All Services</MenuItem>
-                        <MenuItem>Recommendations</MenuItem>
-                    </SubMenu>
-                    <SubMenu title="Help" icon={<FiHelpCircle size={20}/>}>
-                        <MenuItem>FAQs</MenuItem>
-                        <MenuItem>Contact</MenuItem>
-                    </SubMenu>
-                </Menu>
-            </SidebarContent>
-            <SidebarFooter>
-                <Menu iconShape="circle">
-                    <MenuItem icon={<FiSettings/>} size={20}>Settings</MenuItem>
-                </Menu>
-            </SidebarFooter>
-        </ProSidebar>
-    )
+export default function SideNavbar() {
+
+    const [collapsed, setCollapsed] = React.useState("true")
+    function logoClick() {
+        if(collapsed=="false") {
+            setTimeout(()=>{
+                setCollapsed("true")
+            }, 20)   
+        }
+        else {
+            setTimeout(() => {
+                setCollapsed("false")
+            }, 20)
+        }
+    }
+    
+    if(collapsed == "false") {
+        return (
+            <ProSidebar>
+                <SidebarHeader>
+                    <img onClick={logoClick} src={sdscLogoRed} alt="logo" height="50" 
+                    style={{margin:"15px", border:"solid #adadad 2px", borderRadius:"12px"}}/>
+                </SidebarHeader>
+                <SidebarContent>
+                    <Menu iconShape="circle" popperArrow="true">
+                        <MenuItem icon={<TbDashboard size={20}/>}>Dashboard</MenuItem>
+                        <SubMenu title="Home" icon={<TbHome size={20}/>}>
+                            <MenuItem>Current Service</MenuItem>
+                            <MenuItem>Service History</MenuItem>
+                            <MenuItem>Saved</MenuItem>
+                        </SubMenu>
+                        <SubMenu title="Explore" icon={<MdOutlineExplore size={20}/>}>
+                            <MenuItem>All Services</MenuItem>
+                            <MenuItem>Recommendations</MenuItem>
+                        </SubMenu>
+                        <SubMenu title="Help" icon={<FiHelpCircle size={20}/>}>
+                            <MenuItem>FAQs</MenuItem>
+                            <MenuItem>Contact</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+                <SidebarFooter>
+                    <Menu iconShape="circle">
+                        <MenuItem icon={<FiSettings/>} size={20}>Settings</MenuItem>
+                    </Menu>
+                </SidebarFooter>
+            </ProSidebar>
+        )
+    }
+    else {
+        return (
+            <ProSidebar collapsed="true">
+                <SidebarHeader>
+                    <img onClick={logoClick} src={sdscLogoRed} alt="logo" height="50" 
+                    style={{margin:"15px", border:"solid #adadad 1px", borderRadius:"12px"}}/>
+                </SidebarHeader>
+                <SidebarContent>
+                    <Menu iconShape="circle" popperArrow="true">
+                        <MenuItem icon={<TbDashboard size={20}/>}>Dashboard</MenuItem>
+                        <SubMenu title="Home" icon={<TbHome size={20}/>}>
+                            <MenuItem>Current Service</MenuItem>
+                            <MenuItem>Service History</MenuItem>
+                            <MenuItem>Saved</MenuItem>
+                        </SubMenu>
+                        <SubMenu title="Explore" icon={<MdOutlineExplore size={20}/>}>
+                            <MenuItem>All Services</MenuItem>
+                            <MenuItem>Recommendations</MenuItem>
+                        </SubMenu>
+                        <SubMenu title="Help" icon={<FiHelpCircle size={20}/>}>
+                            <MenuItem>FAQs</MenuItem>
+                            <MenuItem>Contact</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+                <SidebarFooter>
+                    <Menu iconShape="circle">
+                        <MenuItem icon={<FiSettings/>} size={20}>Settings</MenuItem>
+                    </Menu>
+                </SidebarFooter>
+            </ProSidebar>
+        )
+    }
+    
 }
 
